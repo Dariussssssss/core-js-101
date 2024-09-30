504abea704b1738b6b3edb33562fb6270979ff0c
Dariusssssssss <sablezzubaya@gmail.com>
2024-09-30 16:47:45 +0300

    add: 14/15, 10/12, 26/31, 9/17

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -486,9 +486,11 @@ function toStringList(arr) {
 //  *      { country: 'Russia',  city: 'Saint Petersburg' }
 //  *    ]
 //  */
-// function sortCitiesArray(/* arr */) {
-//   throw new Error('Not implemented');
+// function sortCitiesArray(arr) {
+//   let countrySorted = arr.forEach((el) => {
+//
 // }
+
 //
 // /**
 //  * Creates an identity matrix of the specified size
@@ -508,8 +510,8 @@ function toStringList(arr) {
 //  *           [0,0,0,1,0],
 //  *           [0,0,0,0,1]]
 //  */
-// function getIdentityMatrix(/* n */) {
-//   throw new Error('Not implemented');
+// function getIdentityMatrix(n) {
+//
 // }
 //
 // /**
@@ -525,9 +527,15 @@ function toStringList(arr) {
 //  *     0, 100 => [ 0, 1, 2, ..., 100 ]
 //  *     3, 3   => [ 3 ]
 //  */
-// function getIntervalArray(/* start, end */) {
-//   throw new Error('Not implemented');
-// }
+function getIntervalArray(start, end) {
+  const res = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = start; i <= end; i++) {
+    res.push(i);
+  }
+  return res;
+}
+
 //
 // /**
 //  * Returns array containing only unique values from the specified array.
@@ -540,9 +548,17 @@ function toStringList(arr) {
 //  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
 //  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
 //  */
-// function distinct(/* arr */) {
-//   throw new Error('Not implemented');
-// }
+function distinct(arr) {
+  const res = [];
+  arr.map((el) => {
+    if (!res.includes(el)) {
+      res.push(el);
+    }
+    return el;
+  });
+  return res;
+}
+
 //
 // /**
 //  * Groups elements of the specified array by key.
@@ -574,9 +590,10 @@ function toStringList(arr) {
 //  *    "Poland" => ["Lodz"]
 //  *   }
 //  */
-// function group(/* array, keySelector, valueSelector */) {
-//   throw new Error('Not implemented');
+// function group(array, keySelector, valueSelector) {
+//
 // }
+
 //
 //
 // /**
@@ -672,8 +689,8 @@ module.exports = {
   findAllOccurrences,
   //   sortCitiesArray,
   //   getIdentityMatrix,
-  //   getIntervalArray,
-  //   distinct,
+  getIntervalArray,
+  distinct,
   //   group,
   selectMany,
 //   getElementByIndexes,
--- test/03-arrays-tests.js
+++ test/03-arrays-tests.js
@@ -659,71 +659,71 @@ describe('03-arrays-tasks', () => {
   //     });
   //   });
   //
-  //
-  //   it.optional('getIntervalArray should return the array of integers from start to end (inclusive)', () => {
-  //     [
-  //       {
-  //         start: 1,
-  //         end: 5,
-  //         expected: [1, 2, 3, 4, 5],
-  //       }, {
-  //         start: -2,
-  //         end: 2,
-  //         expected: [-2, -1, 0, 1, 2],
-  //       }, {
-  //         start: 0,
-  //         end: 100,
-  //         expected: [
-  //           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-  //           20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-  //           40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-  //           60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-  //           80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-  //           100,
-  //         ],
-  //       }, {
-  //         start: 3,
-  //         end: 3,
-  //         expected: [3],
-  //       },
-  //       {
-  //         start: -5,
-  //         end: -3,
-  //         expected: [-5, -4, -3],
-  //       },
-  //     ].forEach((data) => {
-  //       const actual = tasks.getIntervalArray(data.start, data.end);
-  //       assert.deepEqual(
-  //         actual,
-  //         data.expected,
-  //       );
-  //     });
-  //   });
-  //
-  //
-  //   it.optional('distinct should return an array of unique items from the specified array', () => {
-  //     [
-  //       {
-  //         arr: [1, 2, 3, 3, 2, 1],
-  //         expected: [1, 2, 3],
-  //       }, {
-  //         arr: ['a', 'a', 'a', 'a', 'a'],
-  //         expected: ['a'],
-  //       }, {
-  //         arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  //         expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  //       }, {
-  //         arr: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
-  //         expected: [1, 2, 3, 4, 5, 6],
-  //       },
-  //     ].forEach((data) => {
-  //       const actual = tasks.distinct(data.arr);
-  //       assert.deepEqual(
-  //         actual,
-  //         data.expected,
-  //       );
-  //     });
-  //   });
+
+  it.optional('getIntervalArray should return the array of integers from start to end (inclusive)', () => {
+    [
+      {
+        start: 1,
+        end: 5,
+        expected: [1, 2, 3, 4, 5],
+      }, {
+        start: -2,
+        end: 2,
+        expected: [-2, -1, 0, 1, 2],
+      }, {
+        start: 0,
+        end: 100,
+        expected: [
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+          20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+          40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+          60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+          80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+          100,
+        ],
+      }, {
+        start: 3,
+        end: 3,
+        expected: [3],
+      },
+      {
+        start: -5,
+        end: -3,
+        expected: [-5, -4, -3],
+      },
+    ].forEach((data) => {
+      const actual = tasks.getIntervalArray(data.start, data.end);
+      assert.deepEqual(
+        actual,
+        data.expected,
+      );
+    });
+  });
+
+
+  it.optional('distinct should return an array of unique items from the specified array', () => {
+    [
+      {
+        arr: [1, 2, 3, 3, 2, 1],
+        expected: [1, 2, 3],
+      }, {
+        arr: ['a', 'a', 'a', 'a', 'a'],
+        expected: ['a'],
+      }, {
+        arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      }, {
+        arr: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
+        expected: [1, 2, 3, 4, 5, 6],
+      },
+    ].forEach((data) => {
+      const actual = tasks.distinct(data.arr);
+      assert.deepEqual(
+        actual,
+        data.expected,
+      );
+    });
+  });
   //
   //   it.optional('group should return a map of grouped data by key and value selector', () => {
   //     [
